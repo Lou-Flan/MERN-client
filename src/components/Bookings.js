4bb09a60c237c344ae4a8abed72e6eaac06389c0
lou-flan <louise_flanagan@outlook.com>
2020-07-12 15:05:55 +1000

    Moved Bookings component into dashboard. Added some styling to bookings component

--- src/components/Bookings.js
+++ src/components/Bookings.js
@@ -1,17 +1,65 @@
 import React from 'react';
-import Booking from './Booking';
-import { useGlobalState } from '../config/globalState'
+import { useGlobalState } from '../config/globalState';
+import { Link } from 'react-router-dom';
+import { makeStyles } from '@material-ui/core/styles';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
 
 const Bookings = () => {
-	const { store} = useGlobalState()
-	const { bookings } = store
+	const { store } = useGlobalState();
+	const { bookings } = store;
+
+	const useStyles = makeStyles((theme) => ({
+		seeMore: {
+			marginTop: theme.spacing(3),
+		},
+	}));
+
+	const classes = useStyles();
+
+	function preventDefault(event) {
+		event.preventDefault();
+	}
+
 	return (
 		<div>
-			{bookings
-				.sort((a, b) => b.modified_date - a.modified_date)
-				.map((booking) => (
-					<Booking key={booking._id} booking={booking} />
-				))}
+			<Table size="small">
+				<TableHead>
+					<TableRow>
+						<TableCell>Date</TableCell>
+						<TableCell>Name</TableCell>
+						<TableCell>Continent</TableCell>
+						<TableCell>Currency</TableCell>
+						<TableCell align="right">Teeth</TableCell>
+					</TableRow>
+				</TableHead>
+				<TableBody>
+					{bookings
+						.sort((a, b) => b.modified_date - a.modified_date)
+						.map((booking) => (
+							<TableRow key={booking._id}>
+								<TableCell>{booking.modified_date.toLocaleString()}</TableCell>
+								<TableCell>
+									{booking.name} {booking.surname}
+								</TableCell>
+								<TableCell>{booking.continent}</TableCell>
+								<TableCell>{booking.currency}</TableCell>
+								<TableCell align="right">{booking.teeth}</TableCell>
+								<TableCell align="right">
+									<Link to={`/bookings/${booking._id}`}>View</Link>
+								</TableCell>
+							</TableRow>
+						))}
+				</TableBody>
+			</Table>
+			<div className={classes.seeMore}>
+				<Link color="primary" href="#" onClick={preventDefault}>
+					See more orders
+				</Link>
+			</div>
 		</div>
 	);
 };
--- src/components/UserDashboard.js
+++ src/components/UserDashboard.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { useGlobalState } from '../config/globalState';
+import Bookings from './Bookings';
+import Booking from './Booking';
+import EditBooking from './EditBooking';
+import NewBooking from './NewBooking';
 
 const UserDashboard = () => {
 	return (
 		<div>
 			<h1>I am the User Dashboard</h1>
+			<Bookings />
 		</div>
 	);
 };
